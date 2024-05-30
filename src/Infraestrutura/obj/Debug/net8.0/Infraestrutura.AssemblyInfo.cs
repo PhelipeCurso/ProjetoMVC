4321223515ffa2dc2561cd5b728fff253e577d67
Wellisson Reis <Wellissonsilvareis28@hotmail.com>
2024-05-29 23:26:57 -0300

    #1

--- src/Infraestrutura/obj/Debug/net8.0/Infraestrutura.AssemblyInfo.cs
+++ src/Infraestrutura/obj/Debug/net8.0/Infraestrutura.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infraestrutura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ad04e9c54e9cadcf3a1e9495044fa1fc6e9e2e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7d26c40605d8409fbbee2dcebdadd3485ed58e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infraestrutura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infraestrutura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
